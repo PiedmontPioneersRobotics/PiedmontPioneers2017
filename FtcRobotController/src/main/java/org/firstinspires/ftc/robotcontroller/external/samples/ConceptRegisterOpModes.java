a5a3c856688e0857f20ea8dff79e5ba12b1183f3
Anabel-l <32043491+Anabel-l@users.noreply.github.com>
2017-09-24 14:23:39 -0700

    Activate Vuforia

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptRegisterOpModes.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptRegisterOpModes.java
@@ -110,6 +110,7 @@ public class ConceptRegisterOpModes
       // manager.register("Ramp Motor Speed",       ConceptRampMotorSpeed.class);
       // manager.register("Scan Servo",             ConceptScanServo.class);
       // manager.register("Telemetry",              ConceptTelemetry.class);
-      // manager.register("Vuforia Navigation",     ConceptVuforiaNavigation.class);
+      //manager.register("Vuforia Navigation",     ConceptVuforiaNavigation.class);
+      manager.register("VuMark Identification",     ConceptVuMarkIdentification.class);
     }
 }
