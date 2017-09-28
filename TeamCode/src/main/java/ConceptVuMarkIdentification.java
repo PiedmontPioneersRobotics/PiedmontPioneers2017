c2ec19fb30cc62b2b599c71386a89f19830ec085
Alex <alex.hackerboy@gmail.com>
2017-09-27 18:16:37 -0700

    Moved Navigation to team code

--- TeamCode/src/main/java/ConceptVuMarkIdentification.java
+++ TeamCode/src/main/java/ConceptVuMarkIdentification.java
@@ -30,7 +30,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
+import org.firstinspires.ftc.teamcode.ConceptVuforiaNavigation;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
