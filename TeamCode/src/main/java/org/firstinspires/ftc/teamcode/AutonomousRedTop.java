0f620774e0c3d269d85028dc7ad392882ef630b3
XswaglordX <adrumm825@gmail.com>
2017-10-31 17:34:26 -0700

    dog the cometed out code is the bugg child salve

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedTop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedTop.java
@@ -49,6 +49,8 @@ public class AutonomousRedTop extends AutonomousBase {
 
     @Override public void runOpMode() {
         /*
+
+         */
         robot.init(hardwareMap);
         telemetry.addData("Say", "Hello Driver");
         telemetry.update();
@@ -64,6 +66,7 @@ public class AutonomousRedTop extends AutonomousBase {
         vuMark = RelicRecoveryVuMark.from(relicTemplate);
         telemetry.addData(">", "Press Play to start");
         telemetry.update();
+
         waitForStart();
 
         relicTrackables.activate();
@@ -86,37 +89,34 @@ public class AutonomousRedTop extends AutonomousBase {
                 RedTop = false;
             }
             telemetry.update();
-            */
-            /*
-            AddedCode
-            */
-/*
-            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-                //telemetry.addData("VuMark", "%s visible", vuMark);
-                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
-                // telemetry.addData("Pose", format(pose));
-                if (pose != null) {
-                    VectorF trans = pose.getTranslation();
-                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-
-                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
-                    double tX = trans.get(0);
-                    double tY = trans.get(1);
-                    double tZ = trans.get(2);
-
-                    // Extract the rotational components of the target relative to the robot
-                    double rX = rot.firstAngle;
-                    double rY = rot.secondAngle;
-                    double rZ = rot.thirdAngle;
-                }
-            }
-            else {
+//
+//             If the code below is not commented out, the phone locks up.
+//
+//            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+//                //telemetry.addData("VuMark", "%s visible", vuMark);
+//                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+//                // telemetry.addData("Pose", format(pose));
+//                if (pose != null) {
+//                    VectorF trans = pose.getTranslation();
+//                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+//
+//                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
+//                    double tX = trans.get(0);
+//                    double tY = trans.get(1);
+//                    double tZ = trans.get(2);
+//
+//                    // Extract the rotational components of the target relative to the robot
+//                    double rX = rot.firstAngle;
+//                    double rY = rot.secondAngle;
+//                    double rZ = rot.thirdAngle;
+//                }
+//            }
+//            else {
                 // telemetry.addData("VuMark", "not visible");
-            }
+       //     }
 
-            telemetry.update();
         }
-*/
+
     }
 
     String format(OpenGLMatrix transformationMatrix) {
