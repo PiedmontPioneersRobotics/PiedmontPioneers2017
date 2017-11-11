84602239bab77a21da6b9f406101a32cf551c601
christinahsia <hsiajacob@gmail.com>
2017-11-10 16:23:09 -0800

    Same as before

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedTop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedTop.java
@@ -42,7 +42,6 @@ public class AutonomousRedTop extends AutonomousBase {
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate");
-        waitForStart();
         relicTrackables.activate();
         sleep(1000);
 
@@ -99,73 +98,32 @@ public class AutonomousRedTop extends AutonomousBase {
     }
 
     @Override public void runOpMode() {
-        /*
-
-         */
         robot.init(hardwareMap);
         telemetry.addData("Say", "Hello Driver");
-        telemetry.update();
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        parameters.vuforiaLicenseKey = "AVi+Uaj/////AAAAGfWmeyFp9kJor/1TJjz9wLwAbeI4DnCVS28yGBmbfAGBJFycflauxPe49eusMdcCy8oNTAz/0MVmgKGeUKkOcAYysjx4Vu5IqACsLpAv2E4xpJrfCkOyNYAjeY3FVCPweXd+FOczSSS2sBGHbKtxXWBDH+CWCW2xAyesC/xGyY8CepTmYrZMsOm6c9imaGwUzBhZDTZzRmgQ/mxi9rN4UvHEGp0NTKSi72+kn61f8zBy0rDhZ43UjoIwNknCKvyisezzpIBxqynePB3wtANO1g02zj7a8I1AWl0yuMEjfPM5WGdiDm+g85wm9rBqwL2WOKQnC527JVG50ZB4j0RGq3jES/DOfCNESzYCbC+TqpAF";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        vuMark = RelicRecoveryVuMark.from(relicTemplate);
         telemetry.addData(">", "Press Play to start");
         telemetry.update();
-
+        telemetry.addData(">", "Start main loop");
+        telemetry.update();
         waitForStart();
-
-        relicTrackables.activate();
 
         telemetry.addData(">", "Start main loop");
         telemetry.update();
-
-        while (opModeIsActive()) {
-
-            if(RedTop) {
-                holdGlyph();
-                KnockoffJewel("RedTop");
-                driveForward(0.25, 1.28);
-                leftTurn(0.25, 1.4);
-                driveForward(0.25, 0.47);
-                rightTurn(0.25, 1.25);
-                driveForward(0.25, 1.03);
-                dropGlyph();
-                stopDriving();
-                RedTop = false;
-            }
-            telemetry.update();
-//
-//             If the code below is not commented out, the phone locks up.
-//
-//            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-//                //telemetry.addData("VuMark", "%s visible", vuMark);
-//                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
-//                // telemetry.addData("Pose", format(pose));
-//                if (pose != null) {
-//                    VectorF trans = pose.getTranslation();
-//                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-//
-//                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
-//                    double tX = trans.get(0);
-//                    double tY = trans.get(1);
-//                    double tZ = trans.get(2);
-//
-//                    // Extract the rotational components of the target relative to the robot
-//                    double rX = rot.firstAngle;
-//                    double rY = rot.secondAngle;
-//                    double rZ = rot.thirdAngle;
-//                }
-//            }
-//            else {
-                // telemetry.addData("VuMark", "not visible");
-       //     }
-
+        double time_for_driving = checkVuforia();
+        telemetry.addData(">", "Preparing to drive.");
+        telemetry.update();
+        KnockoffJewel("RedTop");
+        driveForward(0.25, time_for_driving);
+        telemetry.addData(">", "Driving forward by:", time_for_driving);
+        telemetry.update();
+        rightTurn(0.25,1.22);
+        telemetry.addData(">", "Turned right");
+        telemetry.update();
+        driveForward(0.25, 1);
+        telemetry.addData(">", "Final drive forward");
+        telemetry.update();
+        dropGlyph();
+        driveBackward(0.25, 0.5);
+        telemetry.update();
         }
 
     }
