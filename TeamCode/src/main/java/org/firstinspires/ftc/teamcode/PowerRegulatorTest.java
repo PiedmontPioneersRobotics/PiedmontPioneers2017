c19a1b289e0876ec4151577f27228f77c459895b
reily <sirocketreily@gmail.com>
2017-12-06 19:12:17 -0800

    Motor regulator test v2.0 w/ bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerRegulatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerRegulatorTest.java
@@ -77,11 +77,8 @@ public class PowerRegulatorTest extends LinearOpMode {
         Right1  = hwMap.get(DcMotor.class, "Right1");
         Right1.setDirection(DcMotor.Direction.FORWARD);
         Right1.setPower(1);
-        dim = hardwareMap.get(DeviceInterfaceModule.class, "dim");   //  Use generic form of device mapping
-        digIn  = hardwareMap.get(DigitalChannel.class, "digin");     //  Use generic form of device mapping
-        digOut = hardwareMap.get(DigitalChannel.class, "digout");    //  Use generic form of device mapping
+        digOut = hwMap.get(DigitalChannel.class, "digout");    //  Use generic form of device mapping
 
-        digIn.setMode(DigitalChannel.Mode.INPUT);          // Set the direction of each channel
         digOut.setMode(DigitalChannel.Mode.OUTPUT);
 
         // wait for the start button to be pressed.
